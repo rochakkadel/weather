9848bbfb833a5388c9329cd525d345c0ea88f104
Rochak Kadel <153967088+rochakkadel@users.noreply.github.com>
2024-04-11 11:27:18 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+const API_KEY = 'e0ebac425e30cb7e26c08e148e1c1b3e';
+module.exports = API_KEY;
---
+++ script.js
@@ -0,0 +1,33 @@
+const API_KEY = require('./config.js');
+
+function getWeather() {
+    const city = document.getElementById("cityInput").value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("cityName").innerText = data.name;
+        document.getElementById("temperature").innerText = `Temperature: ${data.main.temp}°F`;
+        document.getElementById("humidity").innerText = `Humidity: ${data.main.humidity}%`;
+        document.getElementById("emoji").innerText = getWeatherEmoji(data.weather[0].main);
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+function getWeatherEmoji(weather) {
+    switch (weather) {
+        case 'Clear':
+            return '☀️';
+        case 'Clouds':
+            return '☁️';
+        case 'Rain':
+            return '🌧️';
+        case 'Drizzle':
+            return '🌧️';
+        case 'Thunderstorm':
+            return '⛈️';
+        case 'Snow':
+            return '❄️';
+        default:
+            return '🌫️';
+    }
+}
